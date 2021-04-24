7adeac5c1828178276009dc24ddb2f076481b430
myllern <49235951+myllern@users.noreply.github.com>
2021-04-24 12:05:30 +0200

    btn and compass class

---
+++ app/src/main/java/com/example/hellosensor/DisplayCompassActivity.java
@@ -0,0 +1,4 @@
+package com.example.hellosensor;
+
+public class DisplayCompassActivity {
+}
--- app/src/main/java/com/example/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/hellosensor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hellosensor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void openCompass(View view){
+        // When btn is press..
+        Intent intent = new Intent(this, DisplayCompassActivity.class);
+        startActivity(intent);
+
+
+
+
+    }
+
+
 }
