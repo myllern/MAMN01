fc76325bb5d406893ce37f4b8fe451653d7da4c8
myllern <49235951+myllern@users.noreply.github.com>
2021-04-25 13:51:15 +0200

    new solution

--- app/src/main/java/com/example/hellosensor/DisplayCompassActivity.java
+++ app/src/main/java/com/example/hellosensor/DisplayCompassActivity.java
@@ -1,90 +1,157 @@
 package com.example.hellosensor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.os.Vibrator;
 
-public class DisplayCompassActivity extends AppCompatActivity {
-    private ImageView image_compass;
-    private SensorManager sensorManager;
-    private Sensor sensorAccelerometer;
-    private Sensor sensorMagneticField;
-    private TextView textView;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private float[] floatGravity = new float[3];
-    private float[] floatGeoMagnetic = new float[3];
-    private float[] floatOrientation = new float[3];
-    private float[] floatRotationMatrix = new float[9];
+import java.util.Random;
+
+public class DisplayCompassActivity extends AppCompatActivity implements SensorEventListener{
+    ImageView compass_img;
+    View bg;
+    TextView txt_compass;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private Vibrator v;
+    private String last = "";
+    int[] color;
+    MediaPlayer mediaPlayer;
+    static final float ALPHA = 0.25f;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_compass);
-        image_compass = findViewById(R.id.compass_image);
-       // textView = findViewById(R.id.text_compass);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        compass_img =  findViewById(R.id.compass_image);
+        txt_compass = findViewById(R.id.DegreeTv);
+        bg = findViewById(R.id.bg);
+        color = new int[] { Color.BLUE, Color.GRAY, Color.GREEN, Color.YELLOW, Color.CYAN};
+        mediaPlayer = MediaPlayer.create(this, R.raw.pling);
 
-        //create the sensor manager
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-
-        // create sensors
-        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        // create sense SensorEventListener
-        SensorEventListener sensorEventListenerAccelerometer = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-
-                floatGravity =  event.values;
-
-                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity,floatGeoMagnetic);
-                SensorManager.getOrientation(floatRotationMatrix,floatOrientation);
-
-                image_compass.setRotation((float)(-floatOrientation[0]*180/3.14159));
-                // textView.setText((int)(-floatOrientation[0]*180/3.14159));
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-
-        SensorEventListener sensorEventListenerMagneticField = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-
-                floatGeoMagnetic = event.values;
-                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity,floatGeoMagnetic);
-                SensorManager.getOrientation(floatRotationMatrix,floatOrientation);
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-
-
-
-
-
-
-
-
-        sensorManager.registerListener(sensorEventListenerAccelerometer,sensorAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorEventListenerMagneticField,sensorMagneticField,SensorManager.SENSOR_DELAY_NORMAL);
-
+        start();
 
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mLastMagnetometer = lowPass(event.values.clone(),  mLastMagnetometer);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mLastMagnetometer = lowPass(event.values.clone(),  mLastMagnetometer);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+        compass_img.setRotation(-mAzimuth);
+
+        String where = "NW";
+
+        if (mAzimuth >= 350 || mAzimuth <= 10) {
+            v.vibrate(30);
+            int aryLength = color.length;
+
+            Random random = new Random();
+            int rNum = random.nextInt(aryLength);
+
+            bg.setBackgroundColor(color[rNum]);
+            where = "N";
+
+             mediaPlayer.start();
+
+
+        }
+        if (mAzimuth < 350 && mAzimuth > 280)
+            where = "NW";
+        if (mAzimuth <= 280 && mAzimuth > 260)
+            where = "W";
+        if (mAzimuth <= 260 && mAzimuth > 190)
+            where = "SW";
+        if (mAzimuth <= 190 && mAzimuth > 170)
+            where = "S";
+        if (mAzimuth <= 170 && mAzimuth > 100)
+            where = "SE";
+        if (mAzimuth <= 100 && mAzimuth > 80)
+            where = "E";
+        if (mAzimuth <= 80 && mAzimuth > 10)
+            where = "NE";
+
+        txt_compass.setText(mAzimuth + "° " + where);
+
+        last = where;
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected float[] lowPass( float[] input, float[] output ) {
+        if ( output == null ) return input;
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }
+
+    public void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+
+            }
+            else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
 
 
 
